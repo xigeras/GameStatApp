42f7972c4235d8bf127f6d431582fede8236e40a
august b. <69170271+xigeras@users.noreply.github.com>
2025-07-10 02:49:24 +0000

    added comments

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include <iomanip>
+#include <iomanip> //needed for setprecision
 
 using namespace std;
 
 int main() {
+    //declare variables
     string playerName;
     double playerScore;
     int hoursPlayed;
     
-
+    //outputs and inputs to user
     cout << "Player's Name: ";
     cin >> playerName;
 
@@ -18,7 +19,8 @@ int main() {
     cout << "Hours Played: ";
     cin >> hoursPlayed;
     
+    //calculating and displaying the score per hour
     int scorePerHour = playerScore / hoursPlayed;
-    cout << fixed << setprecision(2);
+    cout << fixed << setprecision(2); // Set precision to 2 decimal places
     cout << "Score Per Hour: " << scorePerHour << endl;
 }
